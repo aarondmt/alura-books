65c4060a2206f8ed7986fce176cfcdb32f8c77e6
Aaron Lesbão Dumont <aarondmt@gmail.com>
2024-01-31 15:01:13 -0400

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import Header from './componentes/Header'
-import styled from 'styled-components'
+import Header from "./components/Header";
+import styled from "styled-components";
 
 const AppContainer = styled.div`
-    width: 100vw;
-    height: 100vh;
-    background-image: linear-gradient(90deg,#002F52 35%,#326589 165%);
-`
+  width: 100vw;
+  height: 100vh;
+  background-image: linear-gradient(90deg, #002f52 35%, #326589 165%);
+`;
 
 function App() {
   return (
@@ -15,4 +15,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { createGlobalStyle } from 'styled-components'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -22,11 +22,9 @@ const GlobalStyle = createGlobalStyle`
   li {
     list-style: none;    
   }
+`;
 
-
-`
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <GlobalStyle />
